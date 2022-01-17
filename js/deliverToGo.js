b3c34444401f4e7a8a64f88fe9aad2c8080e6e50
Andrii Yakymiv <a.iakumiv@gmail.com>
2022-01-17 23:32:38 +0200

    Added calculation function.
    Added one-person transaction calculation function. Added function for calculating product delivery transactions.

--- js/deliverToGo.js
+++ js/deliverToGo.js
@@ -1,5 +1,23 @@
-const pricePerDroid = 800;
-const orderedQuantity = 6;
-const deliveryFee = 50;
-const totalPrice = (pricePerDroid * orderedQuantity) + deliveryFee;
-const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`
+// const pricePerDroid = 800;
+// const orderedQuantity = 6;
+// const deliveryFee = 50;
+// const totalPrice = (pricePerDroid * orderedQuantity) + deliveryFee;
+// const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`
+
+function checkForName(fullName, name) {
+    
+    
+    const result = fullName.includes(name);
+    console.log(result)
+    
+    
+
+    
+ 
+ // Change this line
+  return result;
+}
+
+checkForName("Egor Kolbasov", "Egor")
+checkForName("Egor Kolbasov", "egor")
+checkForName("Egor Kolbasov", "egOr")
